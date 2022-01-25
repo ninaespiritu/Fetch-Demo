b1d8011837ddbd27f74b286e5d0366d93855bf44
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-01-25 10:59:43 +0000

    Add request to Cat Fact API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
 
 const App = () => {
+
+  // https://cat-fact.herokuapp.com/facts/random
+
+  const handleFetch = async () => {
+    // store response
+    const response = await fetch("https://cat-fact.herokuapp.com/facts/random")
+    console.log(response)
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello APIs</h1>
+      <button onClick={handleFetch}>Run the Fetch Request</button>
     </div>
   );
 }
